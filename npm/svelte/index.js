a7527b2ab60335fa533d26d47e7784d0684dbf25
Arthur Ydalgo <arthurc@alunos.utfpr.edu.br>
2024-04-20 01:39:09 -0300

    up

--- npm/svelte/index.js
+++ npm/svelte/index.js
@@ -43,7 +43,7 @@ export function createLaravextApp({ nexusResolver, strandsResolver }) {
                         target: nexusElement,
                         props: {
                             laravext
-                        }
+                        },
                     })
 
 
