29b6ab9f2865009b53303b49d655478eb78e71e3
Arthur Ydalgo <arthurc@alunos.utfpr.edu.br>
2024-06-11 12:40:56 -0300

    examples and docs update

--- examples/laravel-11-react/resources/views/sections/home.blade.php
+++ examples/laravel-11-react/resources/views/sections/home.blade.php
@@ -3,7 +3,7 @@
     <main class="mt-6">
         <div class="grid gap-6 lg:grid-cols-2 lg:gap-8">
 
-            <a href="https://github.com/ArthurYdalgo/laravext/tree/main/examples/laravel-10-react" target="_blank"
+            <a href="https://github.com/ArthurYdalgo/laravext/tree/main/examples/laravel-11-react" target="_blank"
                 class="flex items-start gap-4 rounded-lg bg-white p-6 shadow-[0px_14px_34px_0px_rgba(0,0,0,0.08)] ring-1 ring-white/[0.05] transition duration-300 hover:text-black/70 hover:ring-black/20 focus:outline-none focus-visible:ring-[#FF2D20] lg:pb-10 dark:bg-zinc-900 dark:ring-zinc-800 dark:hover:text-white/70 dark:hover:ring-zinc-700 dark:focus-visible:ring-[#FF2D20]">
                 <div class="flex size-12 shrink-0 items-center justify-center rounded-full bg-[#FF2D20]/10 sm:size-16">
                     <img src="/images/logos/react.png" class="w-[100%]" alt="">
