04f94e20dfdd37a617e80d9c57d381b33e0c07fc
Arthur Ydalgo <arthurc@alunos.utfpr.edu.br>
2024-06-11 13:06:55 -0300

    up

--- examples/laravel-11-react/resources/js/nexus/(global)/(auth)/admin/dashboard/page.jsx
+++ examples/laravel-11-react/resources/js/nexus/(global)/(auth)/admin/dashboard/page.jsx
@@ -1,15 +1,19 @@
 import Header from '@/components/Header';
 import PageContent from '@/components/PageContent';
 import { sharedProps } from '@laravext/react'
+import { useTranslation } from 'react-i18next';
 
 export default () => {
+
+    const { t } = useTranslation();
+
     return (
         <>
             <Header>Dashboard</Header>
 
             <PageContent>
                 <div className="p-6 text-gray-900 dark:text-gray-100">
-                    {`You're logged in, ${sharedProps().auth?.user?.first_name}!`}
+                    {t("You're logged in")}, {sharedProps().auth?.user?.first_name}
                 </div>
             </PageContent>
         </>
