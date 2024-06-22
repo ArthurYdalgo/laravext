6b32f927d7290cba7454dbfe6f20ba8b690f8960
Arthur Ydalgo <arthurc@alunos.utfpr.edu.br>
2024-06-21 23:03:24 -0300

    Update 2024_06_21_171847_create_articles_table.php

--- examples/laravel-11-react/database/migrations/2024_06_21_171847_create_articles_table.php
+++ examples/laravel-11-react/database/migrations/2024_06_21_171847_create_articles_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,6 +14,20 @@ return new class extends Migration
     {
         Schema::create('articles', function (Blueprint $table) {
             $table->id();
+
+            $table->foreignIdFor(User::class);
+            $table->string('short_link_code')->index()->unique();
+            $table->string('slug')->index()->unique();
+            $table->string('banner_url', 512)->nullable();
+            $table->string('title');
+            $table->string('subtitle')->nullable();
+            $table->mediumText('content');
+            $table->string('language')->default('en')->index();
+            $table->integer('reading_time')->nullable();
+            $table->text('keywords')->nullable();
+
+            $table->dateTime('published_at')->nullable();
+            $table->softDeletes();
             $table->timestamps();
         });
     }
