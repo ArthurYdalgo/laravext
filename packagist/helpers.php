5a580557ea565969ad5ce3df3e93ecf37e05326c
Arthur Ydalgo <arthur.ydalgo.dev@gmail.com>
2024-08-18 23:03:23 -0300

    removed from this repo, as it's on a dedicated repo now

--- packagist/helpers.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Laravext\Laravext;
-
-if (! function_exists('nexus')){
-    function nexus($page = null, $props = []){
-        return Laravext::nexus($page, $props);
-    }
-}
--- packagist/src/Ssr/SsrException.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Laravext\Ssr;
-
-use Exception;
-
-class SsrException extends Exception
-{
-}
