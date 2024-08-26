e13f9650b116ea9e74abecb0873e7df1bebb9516
Arthur Ydalgo <arthur.ydalgo.dev@gmail.com>
2024-08-25 22:27:05 -0300

    classes fixed in partials/article

--- examples/laravel-11-react/resources/views/partials/article.blade.php
+++ examples/laravel-11-react/resources/views/partials/article.blade.php
@@ -61,7 +61,7 @@
                     @endif
                 </span></div>
             <button type="button"
-                class="inline-flex items-center px-4 py-2 bg-white dark:bg-gray-800 border border-gray-300 dark:border-gray-500 rounded-md font-semibold text-xs text-gray-700 dark:text-gray-300 uppercase tracking-widest shadow-sm hover:bg-gray-50 dark:hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 disabled:opacity-25 transition ease-in-out duration-150 false flex items-center justify-center">
+                class=" px-4 py-2 bg-white dark:bg-gray-800 border border-gray-300 dark:border-gray-500 rounded-md font-semibold text-xs text-gray-700 dark:text-gray-300 uppercase tracking-widest shadow-sm hover:bg-gray-50 dark:hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 disabled:opacity-25 transition ease-in-out duration-150 false flex items-center justify-center">
                 ...
             </button>
             <div class="flex flex-col space-y-2 mt-2"><span
