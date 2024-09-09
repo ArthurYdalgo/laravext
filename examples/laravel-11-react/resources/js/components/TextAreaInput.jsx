ca8033a4edd26010a04ba0b676181175908693d8
Arthur Ydalgo <arthur.ydalgo.dev@gmail.com>
2024-09-08 21:06:43 -0300

    Update TextAreaInput.jsx

--- examples/laravel-11-react/resources/js/components/TextAreaInput.jsx
+++ examples/laravel-11-react/resources/js/components/TextAreaInput.jsx
@@ -21,18 +21,6 @@ export default forwardRef(function TextAreaInput(
     }, []);
 
     return (
-        // <input
-        //     type={type}
-        //     className={
-        //         "border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm " +
-        //         className
-        //     }
-        //     ref={input}
-        //     value={value}
-        //     onChange={(e) => setValue(e.target.value)}
-        //     {...props}
-        // />
-
         <textarea
             className={
                 "border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm " +
