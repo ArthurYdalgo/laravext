f6cbfc9b9f8e7333a353b27252960481efbcf7bd
Arthur Ydalgo <arthur.ydalgo.dev@gmail.com>
2024-09-10 22:44:41 -0300

    bug fix

--- examples/laravel-11-react/resources/js/components/ProfileLink.jsx
+++ examples/laravel-11-react/resources/js/components/ProfileLink.jsx
@@ -5,6 +5,6 @@ export default ({ link }) => {
 
     return <a href={url} target="_blank" className="hover:text-blue-500 cursor:pointer">
         {type === 'icon' && <Fa icon={`fa-${icon} fa-brands`} size='xl'  />}
-        {type === 'short_link' && (<><Fa className="mr-2" icon={`link`} />{href.replace(/^https?:\/\//, '')}</>)}
+        {type === 'short_link' && (<><Fa className="mr-2" icon={`link`} />{url.replace(/^https?:\/\//, '')}</>)}
     </a>;
 };
