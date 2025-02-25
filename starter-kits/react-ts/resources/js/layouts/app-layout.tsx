05c8a5950ede8961a13c158f1a2e121b6607883b
Arthur Ydalgo <25585428+ArthurYdalgo@users.noreply.github.com>
2025-02-24 22:11:25 -0300

    react typescript from official starter kit

---
+++ starter-kits/react-ts/resources/js/layouts/app-layout.tsx
@@ -0,0 +1,13 @@
+import AppLayoutTemplate from '@/layouts/app/app-sidebar-layout';
+import { type BreadcrumbItem } from '@/types';
+
+interface AppLayoutProps {
+    children: React.ReactNode;
+    breadcrumbs?: BreadcrumbItem[];
+}
+
+export default ({ children, breadcrumbs, ...props }: AppLayoutProps) => (
+    <AppLayoutTemplate breadcrumbs={breadcrumbs} {...props}>
+        {children}
+    </AppLayoutTemplate>
+);
---
+++ starter-kits/react-ts/resources/js/layouts/app/app-sidebar-layout.tsx
@@ -0,0 +1,17 @@
+import { AppContent } from '@/components/app-content';
+import { AppShell } from '@/components/app-shell';
+import { AppSidebar } from '@/components/app-sidebar';
+import { AppSidebarHeader } from '@/components/app-sidebar-header';
+import { type BreadcrumbItem } from '@/types';
+
+export default function AppSidebarLayout({ children, breadcrumbs = [] }: { children: React.ReactNode; breadcrumbs?: BreadcrumbItem[] }) {
+    return (
+        <AppShell variant="sidebar">
+            <AppSidebar />
+            <AppContent variant="sidebar">
+                <AppSidebarHeader breadcrumbs={breadcrumbs} />
+                {children}
+            </AppContent>
+        </AppShell>
+    );
+}
