489d570d0b4380dd54561901e797c154cde57ec0
Arthur Ydalgo <25585428+ArthurYdalgo@users.noreply.github.com>
2025-02-26 00:24:03 -0300

    Update page.tsx

--- starter-kits/react-ts/resources/js/nexus/(app)/dashboard/page.tsx
+++ starter-kits/react-ts/resources/js/nexus/(app)/dashboard/page.tsx
@@ -1,7 +1,7 @@
 import { PlaceholderPattern } from '@/components/ui/placeholder-pattern';
 import AppLayout from '@/layouts/app-layout';
 import { type BreadcrumbItem } from '@/types';
-// import { Head } from '@inertiajs/react';
+import { Head } from '@laravext/react';
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
@@ -13,7 +13,7 @@ const breadcrumbs: BreadcrumbItem[] = [
 export default function Dashboard() {
     return (
         <AppLayout breadcrumbs={breadcrumbs}>
-            {/* <Head title="Dashboard" /> */}
+            <Head title="Dashboard" />
             <div className="flex h-full flex-1 flex-col gap-4 rounded-xl p-4">
                 <div className="grid auto-rows-min gap-4 md:grid-cols-3">
                     <div className="border-sidebar-border/70 dark:border-sidebar-border relative aspect-video overflow-hidden rounded-xl border">
