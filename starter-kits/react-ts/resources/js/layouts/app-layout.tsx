70405426cca289e737f070d3eaacf0c003255339
Arthur Ydalgo <25585428+ArthurYdalgo@users.noreply.github.com>
2025-03-02 15:43:18 -0300

    Update app-layout.tsx

--- starter-kits/react-ts/resources/js/layouts/app-layout.tsx
+++ starter-kits/react-ts/resources/js/layouts/app-layout.tsx
@@ -1,4 +1,4 @@
-import AppLayoutTemplate from '@/layouts/app/app-sidebar-layout';
+import AppLayoutTemplate from '@/layouts/app/app-header-layout';
 import { type BreadcrumbItem } from '@/types';
 
 interface AppLayoutProps {
